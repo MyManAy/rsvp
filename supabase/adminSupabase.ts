76f29d7c39145e9960bafbe0742191d38aae90cd
Nithin Monni <monni016@umn.edu>
2024-06-07 10:25:32 -0500

    initial

---
+++ supabase/adminSupabase.ts
@@ -0,0 +1,14 @@
+// import { createClient } from "@supabase/supabase-js";
+// import { Database } from "./types/types_db";
+
+// export const supabase = createClient<Database>(
+//   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//   process.env.SUPABASE_SERVICE_ROLE_KEY!,
+//   {
+//     auth: {
+//       persistSession: false,
+//       autoRefreshToken: false,
+//       detectSessionInUrl: false,
+//     },
+//   }
+// );
